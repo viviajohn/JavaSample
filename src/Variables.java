28c0c72de799912966e2bc8012ea65f3cd0d430a
viviajohn <32187505+viviajohn@users.noreply.github.com>
2020-10-14 10:23:25 +0530

    Learning Variables and datatypes.

---
+++ src/Variables.java
@@ -0,0 +1,19 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myNum = 9;
+		float myFloatNum = 8.99f;
+		char myLetter = 'A';
+		boolean myBool = false;
+		String myText = "Hello World";
+		
+		System.out.println("myNum = " + myNum);
+		System.out.println("myFloatNum = " + myFloatNum);
+		System.out.println("myLetter = " + myLetter);
+		System.out.println("myBool = " + myBool);
+		System.out.println("myText = " + myText);
+	}
+
+}
