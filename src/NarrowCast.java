0f40657816a2028b9bfc34bc60b403703aabaca3
viviajohn <32187505+viviajohn@users.noreply.github.com>
2020-10-14 12:29:44 +0530

    narrowing casting

---
+++ src/NarrowCast.java
@@ -0,0 +1,13 @@
+
+public class NarrowCast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double myDouble = 9.78;
+		int myInt = (int) myDouble;
+		
+		System.out.println("myDouble = " + myDouble);
+		System.out.println("myInt = " + myInt);
+	}
+
+}
