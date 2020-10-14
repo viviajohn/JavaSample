c731473d9f872188f5b178afddedb4b1ff8b2d48
viviajohn <32187505+viviajohn@users.noreply.github.com>
2020-10-14 17:03:41 +0530

    Update Strings.java
    Strings

--- src/Strings.java
+++ src/Strings.java
@@ -3,11 +3,14 @@ public class Strings {
 	public static void main(String[] args)
 	{
 		String myString = "Hello World";
+		String oneString = "Jane";
+		String twoString = "Doe";
 		
 		System.out.println("Length of myString: " + myString.length());
 		System.out.println("Upper Case: " + myString.toUpperCase());
 		System.out.println("Lower Case: " + myString.toLowerCase());
 		System.out.println("Location of World: " + myString.indexOf("World"));
+		System.out.println(oneString + " " + twoString);
 	}
 
 }
