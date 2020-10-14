23b3acab919d301835daaa2b254fe190e70e810d
viviajohn <32187505+viviajohn@users.noreply.github.com>
2020-10-14 12:39:12 +0530

    working with strings

---
+++ src/Strings.java
@@ -0,0 +1,12 @@
+
+public class Strings {
+	public static void main(String[] args)
+	{
+		String myString = "Hello World";
+		
+		System.out.println("Length of myString: " + myString.length());
+		System.out.println("Upper Case: " + myString.toUpperCase());
+		System.out.println("Lower Case: " + myString.toLowerCase());
+	}
+
+}
