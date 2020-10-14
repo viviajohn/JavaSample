3eb4351a67e2e05f07645149f2b05b4d2ca85741
viviajohn <32187505+viviajohn@users.noreply.github.com>
2020-10-14 12:08:59 +0530

    type casting - widening casting

---
+++ src/WideCast.java
@@ -0,0 +1,13 @@
+
+public class WideCast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myInt = 9;
+		double myDouble = myInt;
+		
+		System.out.println("myInt = " + myInt);
+		System.out.println("myDouble = " + myDouble);
+	}
+
+}
