f4524e4f1b97b2240de752016cf3c949b2c8b5b2
viviajohn <32187505+viviajohn@users.noreply.github.com>
2020-10-17 12:46:39 +0530

    Update Strings.java
    Learning Strings

--- src/Strings.java
+++ src/Strings.java
@@ -5,14 +5,18 @@ public class Strings {
 		String myString = "Hello World";
 		String oneString = "Jane";
 		String twoString = "Doe";
+		String x = "10";
+		int y = 20;
+		String z = x+y;
 		
 		System.out.println("Length of myString: " + myString.length());
 		System.out.println("Upper Case: " + myString.toUpperCase());
 		System.out.println("Lower Case: " + myString.toLowerCase());
 		System.out.println("Location of World: " + myString.indexOf("World"));
 		System.out.println(oneString + " " + twoString);
-		
+		System.out.println(oneString.concat(twoString));
 		System.out.println("My name is \"Jane Doe\"");
+		System.out.println("z = " + z);
 	}
 	
 
