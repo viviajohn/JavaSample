d95f8ade7d57fe57502faaeddacee62339ab49ce
viviajohn <32187505+viviajohn@users.noreply.github.com>
2020-10-17 15:12:16 +0530

    mathematical functions
    learning maths functions

---
+++ src/MathFun.java
@@ -0,0 +1,15 @@
+
+public class MathFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.max(5,  10));
+		System.out.println(Math.min(1, 2));
+		System.out.println(Math.sqrt(64));
+		System.out.println(Math.abs(-4.5));
+		System.out.println(Math.random());
+		
+
+	}
+
+}
