ff86f79cb86bec574c982ca533ac6d2b5b30f7b2
viviajohn <32187505+viviajohn@users.noreply.github.com>
2020-10-18 15:27:45 +0530

    Boolean.java file
    Learning boolean

---
+++ src/Boolean.java
@@ -0,0 +1,13 @@
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean isJavaFun = false;
+		boolean isFishTasty = true;
+		System.out.println(isJavaFun);
+		System.out.println(isFishTasty);
+
+	}
+
+}
